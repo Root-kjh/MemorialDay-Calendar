ae4f7434ff324dfb3cbe79a529ccb80b5cb25470
kjh <wldnro3141@gmail.com>
2021-02-10 14:08:21 +0900

    update test settings

--- memorialCalender/user/tests.py
+++ memorialCalender/user/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class UserViewTests(TestCase):
+    
+    def test_signup(self):
+        """
+        test user signup test
+        """
+        
