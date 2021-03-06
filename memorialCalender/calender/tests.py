7803e92f8b296af32ec3a726c812624cd229051b
Drk <wldnro3141@gmail.com>
2021-03-06 14:44:19 +0900

    test code complete

--- memorialCalender/calender/tests.py
+++ memorialCalender/calender/tests.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import User
 from .models import Calender
 import json
 
+UPDATE_CALENDER_DATA = {
+    "title": "new_title",
+    "start_day": "2021-02-01",
+    "cycle_with": "day",
+    "cycle_unit": 10
+}
+
+CREATE_CALENDER_DATA = {
+    "title": "test_title",
+    "start_day": "2021-03-01",
+    "cycle_with": "day",
+    "cycle_unit": 100
+}
+
 class CalenderTests(APITestCase):
 
     def setUp(self):
@@ -16,43 +30,107 @@ class CalenderTests(APITestCase):
     def tearDown(self):
         User.objects.all().delete()
 
+    def create_test_calender(self):
+        response = self.client.post("/calender/", CREATE_CALENDER_DATA)
+
+    def get_other_user_jwt(self):
+        self.client.post("/auth/signup/",{'username': 'dead', 'password': 'pool'})
+        self.user = User.objects.first()
+        response = self.client.post("/auth/signin/", {'username': 'dead', 'password': 'pool'})
+        token = json.loads(response.content)['token']
+        return f'JWT {token}'
+
     def test_create_calender(self):
-        data = {
-            "title": "test_title",
-            "start_day": "2021-03-01",
-            "cycle_with": "day",
-            "cycle_unit": 100
-        }
-        response = self.client.post("/calender/",data)
+        response = self.client.post("/calender/",CREATE_CALENDER_DATA)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Calender.objects.count(), 1)
 
     def test_create_calender_bad_request(self):
-        pass
+        data = {
+            "title": "test_title",
+            "start_day": "2021-03-01",
+            "cycle_with": "day"
+        }
+        response = self.client.post("/calender/",data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Calender.objects.count(), 0)
 
     def test_create_calender_no_jwt(self):
-        pass
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.post("/calender/",CREATE_CALENDER_DATA)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Calender.objects.count(), 0)
+
+    def test_show_calender(self):
+        self.create_test_calender()
+        response = self.client.get("/calender/")
+        response_data = json.loads(response.content)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Calender.objects.first().id, response_data[0]['id'])
 
     def test_show_calender_no_jwt(self):
-        pass
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.get("/calender/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_calender(self):
+        self.create_test_calender()
+        response = self.client.put(f"/calender/{Calender.objects.first().id}/",UPDATE_CALENDER_DATA)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Calender.objects.first().title, UPDATE_CALENDER_DATA['title'])
 
     def test_update_calender_other_user(self):
-        pass
+        self.create_test_calender()
+        other_user_jwt = self.get_other_user_jwt()
+        self.client.credentials(HTTP_AUTHORIZATION=other_user_jwt)
+        response = self.client.put(f"/calender/{Calender.objects.first().id}/",UPDATE_CALENDER_DATA)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Calender.objects.first().title, CREATE_CALENDER_DATA['title'])
 
     def test_update_calender_not_exist(self):
-        pass
+        response = self.client.put(f"/calender/-1/",UPDATE_CALENDER_DATA)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_calender_bad_request(self):
-        pass
+        self.create_test_calender()
+        data = {
+            "title": "new_title",
+            "start_day": "2021-02-01",
+            "cycle_with": "day"
+        }
+        response = self.client.put(f"/calender/{Calender.objects.first().id}/",data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(Calender.objects.first().title, CREATE_CALENDER_DATA['title'])
 
     def test_update_calender_no_jwt(self):
-        pass
+        self.create_test_calender()
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.put(f"/calender/{Calender.objects.first().id}/",UPDATE_CALENDER_DATA)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Calender.objects.first().title, CREATE_CALENDER_DATA['title'])
+
+    def test_delete_calender(self):
+        self.create_test_calender()
+        response = self.client.delete(f"/calender/{Calender.objects.first().id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Calender.objects.count(), 0)
 
     def test_delete_calender_other_user(self):
-        pass
+        self.create_test_calender()
+        other_user_jwt = self.get_other_user_jwt()
+        self.client.credentials(HTTP_AUTHORIZATION=other_user_jwt)
+        response = self.client.delete(f"/calender/{Calender.objects.first().id}/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Calender.objects.count(), 1)
 
     def test_delete_calender_not_exist(self):
-        pass
+        response = self.client.delete(f"/calender/-1/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_calender_no_jwt(self):
-        pass
+        self.create_test_calender()
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.delete(f"/calender/{Calender.objects.first().id}/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Calender.objects.count(), 1)
--- memorialCalender/user/tests.py
+++ memorialCalender/user/tests.py
@@ -1,16 +1,80 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+import json
 
-# Create your tests here.
+PASSWORD_MODIFY_DATA = {"password": "new password"}
 
-class UserViewTests(TestCase):
+class CalenderTests(APITestCase):
+
+    def setUp(self):
+        self.client.post("/auth/signup/",{'username': 'John', 'password': 'complex_password'})
+        self.user = User.objects.first()
+        response = self.client.post("/auth/signin/", {'username': 'John', 'password': 'complex_password'})
+        token = json.loads(response.content)['token']
+        self.user_id = json.loads(response.content)['id']
+        self.client.credentials(HTTP_AUTHORIZATION=f'JWT {token}')
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def get_other_user_jwt(self):
+        self.client.post("/auth/signup/",{'username': 'dead', 'password': 'pool'})
+        self.user = User.objects.first()
+        response = self.client.post("/auth/signin/", {'username': 'dead', 'password': 'pool'})
+        token = json.loads(response.content)['token']
+        return f'JWT {token}'
+
+    def test_password_modify(self):
+        response = self.client.patch(f"/user/{self.user_id}/",PASSWORD_MODIFY_DATA)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(authenticate(username="John", password=PASSWORD_MODIFY_DATA['password']))
+
+    def test_password_modify_other_user(self):
+        other_user_jwt = self.get_other_user_jwt()
+        self.client.credentials(HTTP_AUTHORIZATION=other_user_jwt)
+        response = self.client.patch(f"/user/{self.user_id}/",PASSWORD_MODIFY_DATA)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(authenticate(username="John", password="complex_password"))
+
+    def test_password_modify_not_exist(self):
+        response = self.client.patch("/user/-1/",PASSWORD_MODIFY_DATA)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_password_modify_bad_request(self):
+        data = {"pw": "test"}
+        response = self.client.patch(f"/user/{self.user_id}/",data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue(authenticate(username="John", password="complex_password"))
+
     
-    def test_signup(self):
-        """
-        test user signup test
-        """
-        
-    def test_signin(self):
-        """
-        test user signin test
-        """
-        
+    def test_password_modify_no_jwt(self):
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.patch(f"/user/{self.user_id}/",PASSWORD_MODIFY_DATA)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertTrue(authenticate(username="John", password="complex_password"))
+
+    def test_withdraw(self):
+        response = self.client.delete(f"/user/{self.user_id}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 0)
+
+    def test_withdraw_other_user(self):
+        other_user_jwt = self.get_other_user_jwt()
+        self.client.credentials(HTTP_AUTHORIZATION=other_user_jwt)
+        response = self.client.delete(f"/user/{self.user_id}/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(User.objects.count(), 2)
+
+    def test_withdraw_not_exist(self):
+        response = self.client.delete(f"/user/-1/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(User.objects.count(), 1)
+    
+    def test_withdraw_no_jwt(self):
+        self.client.credentials(HTTP_AUTHORIZATION="no jwt")
+        response = self.client.delete(f"/user/{self.user_id}/")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(User.objects.count(), 1)
+    
